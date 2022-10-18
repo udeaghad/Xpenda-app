1ed6e5825bdfcc2630b3c6de0a80313e98b27c6d
udeaghad <dozie.udeagha@gmail.com>
2022-10-18 14:05:09 +0100

    Add edit for category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -16,9 +16,26 @@ class CategoriesController < ApplicationController
   @category = Category.new(category_params)
   @category.user_id = current_user.id
   if @category.save
-    redirect_to categories_path(current_user)
+    redirect_to categories_path
   else
     render :new
+  end
+end
+
+def edit
+  @category = Category.find(params[:id])
+
+  respond_to do |format|
+    format.html { render:edit, locals: { post: @category}}
+ end
+end
+
+def update 
+  @category = Category.find(params[:id])
+  if @category.update(category_params)
+    redirect_to categories_path
+  else 
+    render :edit 
   end
 end
 
