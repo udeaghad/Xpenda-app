b73abf1ec7126c3fbf7e29fc5ac714c23b1cd7f7
udeaghad <dozie.udeagha@gmail.com>
2022-10-19 20:08:21 +0100

    Perform test for categories request

--- spec/factories/devise.rb
+++ spec/factories/devise.rb
@@ -7,4 +7,20 @@ FactoryBot.define do
     confirmed_at { '2022-10-13 15:41:55.526835' }
     # Add additional fields as required via your User model
   end
+
+  factory :category do
+    id { 1 }
+    name { 'Food' }
+    icon { 'fas fa-utensils' }
+    user_id { 1 }
+    total { 0 }
+  end
+
+  factory :expenditure do
+    id { 1 }
+    name { 'Pizza' }
+    amount { 10 }
+    user_id { 1 }
+    category_id { 1 }
+  end
 end
