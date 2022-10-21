3ae84a1b613dddbf73ba863f28781d951f59f14e
udeaghad <dozie.udeagha@gmail.com>
2022-10-21 13:39:01 +0100

    Update signup authemtication with devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(_resource_or_scope)
     categories_path
   end
+
+  def after_sign_up_path_for(_resource_or_scope)
+    categories_path
+  end
 end
